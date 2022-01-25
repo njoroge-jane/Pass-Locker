7e14e81011c673c5465003a2d17b5fefe6f9db47
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-01-25 06:37:29 +0300

    add save delete display and find accounts methods

---
+++ user.py
@@ -0,0 +1,29 @@
+class User:
+    def __init__(self, account, password):
+        self.account = account
+        self.password = password
+
+    user_passwords = []
+
+    def save_account(self):
+        User.user_passwords.append(self)
+
+    def delete_account(self):
+        User.user_passwords.remove(self)
+
+    @classmethod
+    def display_account(cls):
+        return cls.user_passwords
+
+    @classmethod
+    def find_by_account(cls, accounts):
+        for account in cls.user_passwords:
+            if account.account == accounts:
+                return account
+
+    @classmethod
+    def page_exists(cls, accounts):
+        for account in cls.user_passwords:
+            if account.account == accounts:
+                return account
+        return False
